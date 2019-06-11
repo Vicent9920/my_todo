48b6270298999e9e2abeffb5b890c1371ffc2085
vincent <weixing9920@163.com>
2019-06-11 23:51:41 +0800

    优化首页bottomBar页面

--- lib/page/login/main_page.dart
+++ lib/page/login/main_page.dart
@@ -1,23 +1,21 @@
-
 import 'package:flutter/material.dart';
 import 'package:my_todo/page/login/widget/custom_drawer.dart';
 
 class MainPage extends StatefulWidget {
-
   @override
   State<StatefulWidget> createState() => _MainPageState();
-
 }
-class _MainPageState extends State<MainPage>{
-  String title = "代办清单";
-  int _selectedIndex = 0;
+
+class _MainPageState extends State<MainPage> {
+  var _isDone = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(title),
+        title: Text("${(_isDone) ? '完成' : '待办'}清单"),
         centerTitle: true,
-        leading: IconButton(icon: Icon(Icons.storage), onPressed: (){}),
+        leading: IconButton(icon: Icon(Icons.storage), onPressed: () {}),
       ),
       drawer: CustomDrawer(),
       bottomNavigationBar: BottomAppBar(
@@ -27,22 +25,37 @@ class _MainPageState extends State<MainPage>{
           //均分底部导航栏横向空间
           mainAxisAlignment: MainAxisAlignment.spaceAround,
           children: <Widget>[
-            IconButton(icon: Icon(Icons.border_color)),
+            IconButton(
+              icon: Icon(Icons.border_color,color: (_isDone == false)?Colors.blue:Colors.grey,),
+              onPressed: () {
+                if (_isDone == true) {
+                  setState(() {
+                    _isDone = false;
+                  });
+                }
+              },
+            ),
             SizedBox(), //中间位置空出
-            IconButton(icon: Icon(Icons.done_outline)),
+            IconButton(
+              icon: Icon(Icons.done_outline,color: (_isDone == true)?Colors.orange:Colors.grey,),
+              onPressed: () {
+                if (_isDone == false) {
+                  setState(() {
+                    _isDone = true;
+                  });
+                }
+              },
+            ),
           ],
         ),
-
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-      floatingActionButton: FloatingActionButton( //悬浮按钮
+      floatingActionButton: FloatingActionButton(
+          //悬浮按钮
           child: Icon(Icons.add),
-          onPressed:_onAdd
-      ),
+          onPressed: _onAdd),
     );
   }
 
-  void _onAdd() {
-
-  }
+  void _onAdd() {}
 }
