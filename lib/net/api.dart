e622e61ae1dd33f5a758ace41c3e9b558bce2650
vincent <weixing9920@163.com>
2019-06-28 21:03:06 +0800

    修改部分代码

--- lib/net/api.dart
+++ lib/net/api.dart
@@ -3,26 +3,6 @@ class Api {
   //Base
   static String baseUrl = 'https://www.wanandroid.com/';
 
-  //首页列表
-  static String homelist = 'article/list/';
-
-  //首页banner
-  static String homebanner = 'banner/json/';
-
-  //导航
-  static String navi = 'navi/json';
-
-  //搜索热词
-  static String hotkey = '/hotkey/json';
-
-  //搜索
-  static String search = 'article/query/';
-
-  //公众号列表
-  static String subscriptions = 'wxarticle/chapters/json';
-
-  //查看某个公众号历史数据
-  static String subscriptionsHis = 'wxarticle/list/';
 
   //登录
   static String login = 'user/login';
@@ -33,18 +13,6 @@ class Api {
   //登出
   static String logout = 'user/logout/json';
 
-  //收藏文章列表
-  static String favoriteList = 'lg/collect/list/';
-
-  //收藏
-  static String favorite = 'lg/collect/';
-
-  //取消收藏
-  static String favoriteCancel = 'lg/uncollect_originId/';
-
-  //检测更新
-  static String checkUpdate =
-      'https://raw.githubusercontent.com/sunxiaolei/flutter-wan/master/update.conf';
 
   //TODO列表
   static String todoList = 'lg/todo/v2/list/1/json?status=0';
